8eadc4a7e1145020f3ef47b7ea38de2eb44ee336
Dmitriy.Aleksandrov <Dmitriy.Aleksandrov@tatar.ru>
2023-04-20 00:00:38 +0300

    octokit

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const stripAnsi = require('strip-ansi');
+const uuid = require('uuid');
+
+const { v4 } = uuid;
+const sha = context.payload.pull_request?.head.sha ?? context.sha;
+const { context } = github;
+const { getInput } = core;
+const githubToken = getInput('github-token')
+const actionName = getInput('action-name');
+const octokit = getOctokit(githubToken);
 
 (
     async () => {
         try {
-            core.notice('Calling action works'); 
+            const output = {
+                title: 'Plagiarism Detecotr Results',
+                text: `result text`,
+                // annotations:
+                //     results.map(result => ({
+                //     path: result.path.replace(home, ''),
+                //     start_line: result.location.line,
+                //     end_line: result.location.line,
+                //     start_column: result.location.column,
+                //     end_column: result.location.column,
+                //     annotation_level: 'failure',
+                //     title: result.title,
+                //     message: stripAnsi(result.message)
+                //     }))
+                };
+
+            debug(`[output]${JSON.stringify(output, undefined, 2)}`);
+                
+            return octokit
+                .rest
+                .checks
+                .create({
+                    ...context.repo,
+                    head_sha: sha,
+                    name: stripAnsi(actionName),
+                    conclusion: 'success',
+                    external_id: v4(),
+                    output
+                })
 
         } catch (error) {
             core.setFailed(error.message);
