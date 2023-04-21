2f539fa8ea09f55cc06190be2c2ba4e0a3786922
Dmitriy.Aleksandrov <Dmitriy.Aleksandrov@tatar.ru>
2023-04-21 14:19:02 +0300

    t

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-import { debug, getInput, setFailed } from "@actions/core";
-import { context } from "@actions/github";
+import { debug, setFailed } from "@actions/core";
 import fs from 'fs';
-
-const sha = context.payload.pull_request?.head.sha ?? context.sha;
-const githubToken = getInput('github-token')
-const actionName = getInput('action-name');
-// const octokit = getOctokit(githubToken);
 
 (
     async () => {
         try {
             const data = fs.readFileSync('./plag.json', { encoding:'utf8', flag:'r' });
             console.log(data);
-
-            debug(`[output]${JSON.stringify(output, undefined, 2)}`);
 
         } catch (error) {
             setFailed(error.message);
